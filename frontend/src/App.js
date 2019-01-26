ae8d954bbdb2194f438b3d419d8c482b9354b1da
Sanketh Hegde <san2heg@gmail.com>
2019-01-26 12:32:12 -0800

    Setup frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Top from './Top.jsx'
+
+class Side extends React.Component {
+  render() {
+    return (
+      <div>SIDE</div>
+    );
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>MAIN</div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="container">
+        <div className="topbar">
+          <Top />
+        </div>
+        <div className="flex-container">
+          <div className="sidebar">
+            <Side />
+          </div>
+          <div className="content">
+            <Main />
+          </div>
+        </div>
       </div>
     );
   }
