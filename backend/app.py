fce0a8d99f8d6304e1c9293193b1451b25ebc2ad
Rajiv Anisetti <rajiv.anisetti@gmail.com>
2019-01-26 13:12:40 -0800

    added preprocessing

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,23 @@ from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd 
 import matplotlib as mpl
-
+from data.preprocess import Preprocessor
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/')
     def home():
-        return "Let's get it boy"
+        preprocessor = Preprocessor()
+        df = preprocessor.generate_data()
+        print(df.head())
+        return jsonify(df.values.tolist())
     
     return app
     
 
-
 if __name__ == "__main__":
     app = create_app()
     app.run(port = 7000)
+
     
