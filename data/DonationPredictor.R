6f123aa9ea16d1f12f20674281f45b748184cd2b
Ritesh Pendekanti <riteshkp@hotmail.com>
2019-01-26 19:02:10 -0800

    Add files via upload

--- data/DonationPredictor.R
+++ data/DonationPredictor.R
@@ -48,53 +48,58 @@ Farmer <<- 64170
 Social_Worker <<- 45900
 Pharmacist <<- 115000
 
+#Global Constants
+FEMALE <<- 0 
+PREV_DONOR <<- 1
+
 #Import data
-data <- read.csv("md-haw.csv",header=T)
-id <- as.integer(data[[1]])
-first_name <- as.character(data[[2]])
-last_name <- as.character(data[[3]])
-gender <- as.character(data[[4]])
-prev_donor <- as.character(data[[5]])
-is_bilingual <- as.character(data[[6]])
-occupation <- as.character(data[[7]])
-age <- as.integer(data[[8]])
+data <- read.csv("mock_data - processed_data.csv",header=T)
+first_name <- as.character(data[[1]])
+last_name <- as.character(data[[2]])
+gender <- as.integer(data[[3]])
+prev_donor <- as.integer(data[[4]])
+is_bilingual <- as.integer(data[[5]])
+occupation <- as.character(data[[6]])
+age <- as.integer(data[[7]])
 
 #Preallocate
 score <- rep(NA, 1000)
+donation <- rep(NA, 1000)
 flips <- rep(NA, 1000)
 
 calcDonationScore <- function(gender, prevDonor, occupation, age){
-        genderWeight <- 0.2
-        prevDonorWeight <- 0.2
-        occupationWeight <- 0.3
-        ageWeight <- 0.3
-        randomWeight <- runif(1, -0.3, 0.3)
-        
+        genderWeight <- 0.05
+        prevDonorWeight <- 0.10
+        occupationWeight <- 0.9
+        ageWeight <- 0.15
+        randomWeight <- runif(1, -0.05, 0.05)
+
         #https://www.thirdsector.co.uk/women-likely-donate-men-iof-study-finds/fundraising/article/1442144
-        if(gender == "Female"){
+        #Female = 0, Male = 1
+        if(gender == FEMALE){
                 genderWeight <- genderWeight * 0.54
         } else {
                 genderWeight <- genderWeight * 0.40
         }
-        
-        if(prevDonorWeight == "TRUE"){
-                prevDonorWeight <- prevDonorWeight * 0.6
+        #
+        if(prevDonorWeight == PREV_DONOR){
+                prevDonorWeight <- prevDonorWeight * 0.65
         } else {
-                prevDonorWeight <- prevDonorWeight * 0.4
+                prevDonorWeight <- prevDonorWeight * 0.35
         }
         
         #https://www.usatoday.com/story/money/taxes/2018/07/06/how-much-average-taxpayer-give-charity-taxes-2018/36561381/
         if(occupation < 30000){
-                occupationWeight <- occupationWeight * 0.22
+                occupationWeight <- occupationWeight * 0.05
         } else if (occupationWeight < 50000){
-                occupationWeight <- occupationWeight * 0.38
+                occupationWeight <- occupationWeight * 0.23
         } else if (occupationWeight < 75000){
-                occupationWeight <- occupationWeight * 0.44
+                occupationWeight <- occupationWeight * 0.33
         } else if (occupationWeight < 90000){
-                occupationWeight <- occupationWeight * 0.51
+                occupationWeight <- occupationWeight * 0.46
         } else if (occupationWeight < 110000){ 
                 occupationWeight <- occupationWeight * 0.61
-        } else if (occupationWeight < 150000){
+        } else if (occupationWeight < 130000){
                 occupationWeight <- occupationWeight * 0.70
         } else {
                 occupationWeight <- occupationWeight * 0.81
@@ -102,40 +107,43 @@ calcDonationScore <- function(gender, prevDonor, occupation, age){
         
         #https://www.statista.com/statistics/292936/giving-to-charity-in-england-uk-y-on-y-by-age/
         if(age < 24){
-                ageWeight <- ageWeight * 0.57
+                ageWeight <- ageWeight * 0.10
         } else if(age < 34){
-                ageWeight <- ageWeight * 0.69
+                ageWeight <- ageWeight * 0.35
         } else if(age < 49){
-                ageWeight <- ageWeight * 0.75
+                ageWeight <- ageWeight * 0.67
         } else if(age < 64){
                 ageWeight <- ageWeight * 0.80
         } else if(age < 74){
-                ageWeight <- ageWeight * 0.85
+                ageWeight <- ageWeight * 0.78
         } else {
                 ageWeight <- ageWeight * 0.83
         }
         
         total <- genderWeight + prevDonorWeight + occupationWeight + ageWeight + randomWeight
         
-        if(total > 1){
-                total <- 1
-        }
-        
         return (total)
 }
 
 for(i in 1:1000){
         score[i] <- calcDonationScore(gender[i], prev_donor[i], occupation[i], age[i])
-        flips[i] <- sample(c(0,1), 
-                        size = 1, 
-                        replace = TRUE, 
-                        prob = c(1 - score[i],score[i]))
-        
+        #flips[i] <- sample(c(0,1), 
+        #                size = 1, 
+        #               replace = TRUE, 
+        #                prob = c(1 - score[i],score[i]))
+}
+
+for(i in 1:1000){
+        if(score[i] > quantile(score, .375)){
+                donation[i] <- 1
+        } else {
+                donation[i] <- 0
+        }
 }
 
 
 #Write to CSV
-output <- cbind(score, flips)
+output <- cbind(score, donation)
 write.csv(output, file = "output.csv", quote = FALSE)
 
 
